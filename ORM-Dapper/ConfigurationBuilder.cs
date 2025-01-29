123358c48eb539c05742155c9a18f5fb4091f3c5
Lydia Zamora <lydiazamorayoga@gmail.com>
2025-01-28 21:29:34 -0800

    configuring main method

---
+++ ORM-Dapper/ConfigurationBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace ORM_Dapper
+{
+    internal class ConfigurationBuilder
+    {
+        public ConfigurationBuilder()
+        {
+        }
+
+        internal object SetBasePath(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
