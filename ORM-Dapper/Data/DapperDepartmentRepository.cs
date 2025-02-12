8e8495d0b5ec64aae80fc47feb6cbaeff1cc56b7
Lydia Zamora <lydiazamorayoga@gmail.com>
2025-02-12 10:40:10 -0800

    dep repo set up

---
+++ ORM-Dapper/Data/DapperDepartmentRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using Dapper;
+using ORM_Dapper;
+using ORM_Dapper.Data;
+
+namespace ORM_Dapper
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public void AddDepartment(string name)
+        {
+            _connection.Execute(sql: "INSERT INTO departments (Name) Values (@name);", param: new {name});
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>(sql: "SELECT * FROM departments;");
+        }
+    }
+}
+
---
+++ ORM-Dapper/Data/IDepartmentRepository.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace ORM_Dapper.Data
+{
+	public interface IDepartmentRepository
+	{
+	}
+}
+
